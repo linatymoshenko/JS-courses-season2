af04446fc2dda5fd5a9c21ebc60a417dc3bdcd9d
Lina Tymoshenko <linochkato@gmail.com>
2018-09-28 16:37:23 +0300

    create functions sumArrayElements and sortArrayAscending

---
+++ linaTymoshenko_linatymoshenko/sortArrayAscending.js
@@ -0,0 +1,28 @@
+"use strict";
+
+function sortArrayAscendingUseSort(array) {
+  function compareNumbers(number1, number2) {
+    return number1 - number2;
+  }
+
+  array.sort(compareNumbers);
+
+  return array;
+}
+
+// sortArrayAscendingUseSort([2, 67, 65, -8, 42, 3, 9]);
+
+function sortArrayAscendingUseFor(array) {
+  for (var firstIndex = 0; firstIndex < array.length; firstIndex++) {
+    for (var secondIndex = 0; secondIndex < array.length - 1 - firstIndex; secondIndex++) {
+      if (array[secondIndex] > array[secondIndex + 1]) {
+        var temporary = array[secondIndex];
+        array[secondIndex] = array[secondIndex + 1];
+        array[secondIndex + 1] = temporary;
+      }
+    }
+  }
+  return array;
+}
+
+// sortArrayAscendingUseFor([10, 9, 8, 7, 6, 5, 4, 3]);
---
+++ linaTymoshenko_linatymoshenko/sumArrayElements.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function sumArrayElements(array) {
+  var sum = 0;
+
+  for (var index = 0; index < array.length; index++) {
+    sum += array[index];
+  }
+
+  return sum;
+}
+
+// sumArrayElements([1, 4, 6, 7, 9, 3]);

47555d737bd6b3de93fcac7286368db3c1f727c2
Lina Tymoshenko <linochkato@gmail.com>
2018-09-29 13:01:44 +0300

    create function getUniqueArrayElements

---
+++ linaTymoshenko_linatymoshenko/getUniqueArrayElements.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function getUniqueArrayElements(array) {
+  var result = [];
+
+  for (var index = 0; index < array.length; index++) {
+    if (result.indexOf(array[index]) == -1) {
+      result.push(array[index]);
+    }
+  }
+
+  return result;
+}
+
+// getUniqueArrayElements([1, 1, 1, 3, 2, 2]);

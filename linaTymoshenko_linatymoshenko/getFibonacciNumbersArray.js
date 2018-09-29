84574862b6609fb96752ecc0d85393164228622e
Lina Tymoshenko <linochkato@gmail.com>
2018-09-29 13:33:37 +0300

    create function getFibonacciNumbersArray

---
+++ linaTymoshenko_linatymoshenko/getFibonacciNumbersArray.js
@@ -0,0 +1,21 @@
+"use strict";
+
+function getFibonacciNumbersArray(countNumbers) {
+  var result = [];
+
+  if (countNumbers >= 1) {
+    result.push(0);
+  }
+
+  if (countNumbers >= 2) {
+    result.push(1);
+  }
+
+  while (result.length < countNumbers) {
+    result.push(result[result.length - 1] + result[result.length - 2])
+  }
+
+  return result;
+}
+
+// getFibonacciNumbersArray(8);

4e47632eb23fb9972816a03747c52a8055520dd7
Lina Tymoshenko <linochkato@gmail.com>
2018-10-06 18:50:44 +0300

    create object User

---
+++ linaTymoshenko_linatymoshenko/User.js
@@ -0,0 +1,39 @@
+let user = {};
+
+Object.defineProperties(user, {
+  name: {
+    value: "John Doe"
+  },
+
+  age: {
+    value: 30
+  },
+
+  profession: {
+    value: "doctor"
+  },
+
+  userInfo: {
+    get: () => "My name is " + user.name + ". I am " + user.age + " years old. I am a " + user.profession
+  },
+
+  businessCard: {
+    get: () => (new BusinessCard(user.name, user.profession))
+  }
+});
+
+function BusinessCard(name, profession) {
+  this.name = name;
+  this.profession = profession;
+}
+
+user.getAnyUserInfo = function (property) {
+  return this[property];
+};
+
+console.log(user.userInfo);
+console.log(user.businessCard);
+console.log(user.name);
+console.log(user.age);
+console.log(user.profession);
+console.log(user.getAnyUserInfo("name"));

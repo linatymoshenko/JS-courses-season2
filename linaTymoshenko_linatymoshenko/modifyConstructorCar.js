f22c4c0dc511f30bc697fcbda1ae4fd246061142
Lina Tymoshenko <linochkato@gmail.com>
2018-10-07 18:42:49 +0300

    delete extra logic

--- linaTymoshenko_linatymoshenko/modifyConstructorCar.js
+++ linaTymoshenko_linatymoshenko/modifyConstructorCar.js
@@ -3,12 +3,7 @@ const DEFAULT_OWNER_FULL_NAME = "John Doe";
 function Car(brand, fuelTank, ownerFullName = DEFAULT_OWNER_FULL_NAME) {
   this.brand = brand;
   this.fuelTank = fuelTank;
-  this.ownerFullName = DEFAULT_OWNER_FULL_NAME;
-
-  if (ownerFullName.indexOf(" ") > -1) {
-    this.ownerFullName = ownerFullName;
-  }
-
+  this.ownerFullName = ownerFullName;
   let [firstName, lastName] = this.ownerFullName.split(" ");
   this.getFirstNamePart = () => firstName;
   this.getSecondNamePart = () => lastName;
